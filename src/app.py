035c1303560a21db95bd654d316323eedd3303e5
Ieschua <ieschua@outlook.com>
2021-06-01 02:13:03 -0500

    holo xd

--- src/app.py
+++ src/app.py
@@ -15,7 +15,6 @@ from flask_marshmallow import Marshmallow
 
 #endregion
 
-
 #region Configuración de conexión
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:hC5K*M0OSvrNjxaI@localhost/dbregasi' #Cadena de conexion
